8ea969f68bfd998d4529a523cf06d0435311782b
Aaron Ray <aaray619@gmail.com>
2020-09-27 11:20:53 -0500

    Added code that creates random characters and puts them together

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,47 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+var length = parseInt(prompt ("How many characters do you want your password to be? (please enter a value between 8 and 128)"));
+
+if (length < 8){
+  alert("Please enter a value between 8 and 128");
+}
+else if (length >128){
+  alert("Please enter a value between 8 and 128");
+}
+
+
+
+function getRandomLetter(){
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var  randomLetter = letters[Math.floor(Math.random() * letters.length)]; 
+return randomLetter;
+}
+function getCapitalLetter(){
+return getRandomLetter().toUpperCase();
+}
+function getRandomNumber(){
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+  var  randomNumber = numbers[Math.floor(Math.random() * numbers.length)]; 
+  return randomNumber;
+}
+function getRandomSpecial(){
+  var special = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","]","/","?","~","`","<",">"]
+  var randomSpecial = special[Math.floor(Math.random() * special.length)];
+  return randomSpecial;
+}
+function getRandomCharacter(){
+  var character =[getRandomNumber(), getRandomSpecial(), getRandomLetter(), getCapitalLetter()];
+  var randomCharacter = character[Math.floor(Math.random() * character.length)];
+
+  return randomCharacter;
+}
+var randomPassword = [];
+for(i=0; i < length; i++){
+  randomPassword.push(getRandomCharacter());
+}
+var finalPassword = randomPassword.join("");
+console.log(finalPassword);
